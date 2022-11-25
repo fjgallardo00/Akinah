e1349d9eb9b5278b40e7a2c6172ee072e6e4c286
JoseCarlosJC <jcarloslopez@correo.ugr.es>
2022-11-25 18:26:36 +0100

    Clasificación propia de los libros, close #10
    Se han añadido varias clasificaciones para los libros. Se ha usado un objecto de Javascript con pares clave-valor, emulando un enumerado. De esta manera podemos usar la notación punto (.) para acceder a las claves (comodidad). Se usa el objeto Symbol para obtener valores únicos, para evitar un posible error futuro si se hacen comparaciones entre distintos enumerados que por casualidad tienen valores iguales

---
+++ src/model/generos.js
@@ -0,0 +1,13 @@
+const Generos = Object.freeze({
+   Ficcion: Symbol("ficcion"),
+   Romantico: Symbol("romantico"),
+   Terror: Symbol("terror"),
+   Adolescente: Symbol("adolescente"),
+   Historia: Symbol("historia"),
+   Adultos: Symbol("adultos"),
+   Dramatico: Symbol("dramatico"),
+   Lirica: Symbol("lirica"),
+   Didactico: Symbol("didactico"),
+   Internacional: Symbol("internacional"),
+   Otros: Symbol("otros")
+})
