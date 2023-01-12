0c0e5fbadcc5a928211f1755e50567c71ad6bfbf
JoseCarlosJC <jcarloslopez@correo.ugr.es>
2023-01-12 10:47:55 +0100

    Delete atributo descripcion; close #12
    En ningún lado de la HU1 #7 se meciona la descripción, solo los gustos del usuario, que por la definición del problema, se basan en los géneros

--- src/model/libro.js
+++ src/model/libro.js
@@ -2,26 +2,23 @@ import {ConjuntoGeneros} from "./generos.js"
 
 /**
  * @class Libro Representa un libro
- * Encapsula la información del título, autor, descripción y gérenos
+ * Encapsula la información del título, autor y gérenos
  * que definen a un libro
  */
 export class Libro{
    #titulo
    #autor
-   #descripcion
    #generos
 
    /**
     * @constructor
     * @param {string} titulo - Titulo del libro
     * @param {string} autor - Autor del libro
-    * @param {string} descripcion - Descripción del libro
     * @param {ConjuntoGeneros} generos - Géneros asociados al libro
     */
-   constructor(titulo, autor, descripcion, generos){
+   constructor(titulo, autor, generos){
       this.#titulo = titulo
       this.#autor = autor
-      this.#descripcion = descripcion
       this.#generos = generos
    }
 
@@ -35,12 +32,9 @@ export class Libro{
    }
 
 
-   get descripcion(){
-      return this.#descripcion
-   }
-
    /**
-    * Devuelve el conjunto (Set) de géneros asociados al libro
+    * @returns {Set} - Devuelve el conjunto de géneros asociados al libro.
+    * Los valores del conjunto son strings
     */
    get generos(){
       return this.#generos.generos // getter de ConjuntoGeneros
